d989539725a34e63c04a87a302512644eb49c516
Darthirius <ssxanty_91@hotmail.com>
2022-06-04 14:41:43 +0200

    commit ejercicio 6 parte 1

---
+++ Ejercicio 6 parte 1 Santiago De Vicente.cpp
@@ -0,0 +1,80 @@
+// Examen IDE Santiago De Vicente.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+//Ejercicio 6 Parte 1//
+
+public class (Profesor{
+	int clave_prof;
+string nombre;
+string apellido;
+string especialidad;
+string cargo;
+
+//constructor clase profesor, sin parametros
+public Profesor() {
+
+
+}
+
+//constructor clase profesor, con parametros
+public profesor(int clave_prof, string nombre, string apellido, string especialidad, string cargo)
+
+public int getClave_prof() {
+
+}
+
+public void setClave_prof(int clave_prof) {
+
+
+}
+
+public string getNombre() {
+
+
+}
+
+public void setNombre(string nombre) {
+
+
+}
+
+public string getApellido() {
+
+
+}
+
+public void setApellido(string apellido) {
+
+}
+
+public string getEspecialidad(){
+
+
+}
+
+public void setEspecialidad(string especialidad) {
+
+}
+
+public string getCargo() {
+
+
+}
+
+public void setCargo(string cargo) {
+
+}
+
+public string dictarClase() {
+
+
+}
+
+public int calificarExamenes() {
+
+
+}
+
+
+}
+
